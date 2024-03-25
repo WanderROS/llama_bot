f97a5560d4b52f9c2d2b692de48f168eb5bc966e
WanderROS <wsp1194409532@gmail.com>
2024-03-25 20:29:33 +0800

    捕获 Ctrl-C 关闭激光雷达

--- llama_ws/src/tarkbot_robot/src/laser_on.cpp
+++ llama_ws/src/tarkbot_robot/src/laser_on.cpp
@@ -1,5 +1,7 @@
 #include "laser_on.h"
+#include <csignal>
 
+bool flg_exit = false;
 /*
  * @功  能  主函数，ROS初始化，调用构造函数初始化
  */
@@ -11,10 +13,17 @@ int main(int argc, char **argv)
     LaserOn laserOn;
 }
 
+void SigHandle(int sig)
+{
+    flg_exit = true;
+    ROS_WARN("catch CTRL-C Signal!");
+}
+
 LaserOn::LaserOn()
 {
     ros::NodeHandle nh;
     nh_ = nh;
+    signal(SIGINT, SigHandle);
     laser_on_off_pub_ = nh.advertise<std_msgs::Int8>("laser_on_off", 10);
     ros::Duration(3).sleep();
     laser_on_off_msgs_.data = 1;
@@ -22,6 +31,26 @@ LaserOn::LaserOn()
     {
         laser_on_off_pub_.publish(laser_on_off_msgs_);
         ROS_INFO("Ydliar Laser Turn On Success!");
+    }
+    // 捕获到 Ctrl-C 后关闭激光雷达
+    ros::Rate rate(1000);
+    while (ros::ok())
+    {
+        if (flg_exit)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                i++;
+                laser_on_off_msgs_.data = 0;
+                laser_on_off_pub_.publish(laser_on_off_msgs_);
+                rate.sleep();
+            }
+            // 提示信息
+            ROS_INFO("Ydliar Laser Turn Off....");
+            ros::shutdown();
+        }
+        rate.sleep();
     }
     ros::spin();
 }
